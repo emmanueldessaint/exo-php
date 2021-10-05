83839e99c0bd60c83636672f41e2ca774921c275
Emmanuel DESSAINT <e.dessaint@r3mscore.com>
2021-10-05 11:30:59 +0200

    plus ou moins fonctionne

---
+++ plusoumoins.php
@@ -0,0 +1,31 @@
+<?php
+var_dump($argv);
+
+$min = $argv[1];
+
+$max = $argv[2];
+
+$vies = $argv[3];
+
+$myNumber = (rand($min,$max)) . PHP_EOL;
+
+var_dump($myNumber);
+
+for ($i=0; $i < $vies; $i++) {
+    $line = readline("Commande : ");
+    readline_add_history($line);
+    
+    if ($line < $myNumber) {
+        echo "Le chiffre est plus grand" . PHP_EOL;
+    }
+    if ($line > $myNumber) {
+        echo "Le chiffre est moins grand" . PHP_EOL;      
+    }
+    if ($line == $myNumber) {
+        echo "Vous avez trouvé le chiffre exact" . PHP_EOL;
+        die();
+    }
+}
+
+
+?>
